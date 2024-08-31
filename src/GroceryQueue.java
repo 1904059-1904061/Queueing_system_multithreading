6f942c4a7f3141f17653969cc9297f5674b6e8a0
SA-K1B <tareqshakib789@gmail.com>
2024-08-31 15:31:34 +0600

    adding customer in the queue

--- src/GroceryQueue.java
+++ src/GroceryQueue.java
@@ -8,9 +8,39 @@ public class GroceryQueue {
     private final Queue<Customer> queue;
     private final int maxLength;
     private final int numQueues;
+    private int served = 0;
+    private int left = 0;
+    private final Lock lock;
+    private final Condition notEmpty;
     public GroceryQueue(int numQueues,int maxLength){
         this.queue = new LinkedList<>();
         this.maxLength = maxLength;
         this.numQueues = numQueues;
     }   
+    public boolean addCustomer(Customer customer) {
+        lock.lock();
+        try {
+            if (queue.size() < maxLength) {
+                queue.offer(customer);
+                return true;
+            } else {
+                return false;
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+    public Customer getNextCustomer() throws InterruptedException {
+        lock.lock();
+        try {
+            while (queue.isEmpty()) {
+                if (!queue.isEmpty()) {
+                    return queue.poll();
+                }
+            }
+            return queue.poll();
+        } finally {
+            lock.unlock();
+        }
+    }
 }
