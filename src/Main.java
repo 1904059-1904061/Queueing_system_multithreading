794f65eeab19fe07926e837465782bbda3627e13
SA-K1B <tareqshakib789@gmail.com>
2024-09-01 16:04:23 +0600

    adding customer to grocery queue

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int tellers = 3;
-        int maxLength = 5;
+        int bankmaxLength = 5;
+        int grocerymaxLength = 2;
+        int numQueues = 3 ;
         int simulationMinutes = 2;
         int simulationSeconds = simulationMinutes * 5;
-        BankQueue bankQueue = new BankQueue(tellers,maxLength);
-        QueueSimulator simulator = new QueueSimulator(bankQueue,simulationSeconds);
+        GroceryQueue groceryQueue = new GroceryQueue(numQueues, grocerymaxLength);
+        BankQueue bankQueue = new BankQueue(tellers,bankmaxLength);
+        QueueSimulator simulator = new QueueSimulator(bankQueue,groceryQueue,simulationSeconds);
         simulator.startSimulation();
     }
 }
--- src/QueueSimulator.java
+++ src/QueueSimulator.java
@@ -2,30 +2,34 @@ import java.util.Random;
 
 public class QueueSimulator {
     private final BankQueue bankQueue;
+    private final GroceryQueue groceryQueue;
     private final int simulationTime; // in seconds
     private final Random random = new Random();
     private volatile boolean simulationRunning = true;
     private int tellers ;
     private int k = 0;
-    public QueueSimulator(BankQueue bankQueue, int simulationTime){
+    public QueueSimulator(BankQueue bankQueue,GroceryQueue groceryQueue ,int simulationTime){
         this.bankQueue = bankQueue;
+        this.groceryQueue = groceryQueue;
         this.simulationTime = simulationTime;
     }
     public void startSimulation() {
         Thread simulationThread = new Thread(this::simulate);
-        Thread customerArrivalThread = new Thread(this::customerArrival);
+        Thread bankcustomerArrivalThread = new Thread(this::bankcustomerArrival);
+        Thread grocerycustomerArrivalThread = new Thread(this::grocerycustomerArrival);
         tellers = bankQueue.getTeller();
         Thread[] tellerThreads = new Thread[tellers];
         simulationThread.start();
-        customerArrivalThread.start();
-
+        bankcustomerArrivalThread.start();
+        grocerycustomerArrivalThread.start();
         for (int i = 0; i < tellers; i++) {
             tellerThreads[i] = new Thread(this::serveCustomer);
             tellerThreads[i].start();
         }
         try {
             simulationThread.join();
-            customerArrivalThread.join();
+            bankcustomerArrivalThread.join();
+            grocerycustomerArrivalThread.join();
             for (Thread tellerThread : tellerThreads) {
                 tellerThread.join();
             }
@@ -50,11 +54,24 @@ public class QueueSimulator {
         System.out.println("Remaining Customer in the queue: " + bankQueue.queueSize());
     }
 
-    private void customerArrival() {
+    private void bankcustomerArrival() {
         while (simulationRunning) {
             Customer customer = new Customer(k);
             if (!bankQueue.addCustomer(customer)) {
                 bankQueue.incrementLeft();
+            }
+            try {
+                Thread.sleep((2 + random.nextInt(5)) * 1000L); // sleep for 2 to 6 seconds
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+    private void grocerycustomerArrival() {
+        while (simulationRunning) {
+            Customer customer = new Customer(k);
+            if (!groceryQueue.addCustomer(customer)) {
+                
             }
             try {
                 Thread.sleep((2 + random.nextInt(5)) * 1000L); // sleep for 2 to 6 seconds
