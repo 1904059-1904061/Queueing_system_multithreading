92ccc3adadf96e6b9684550c6f042f4236bf988d
SA-K1B <tareqshakib789@gmail.com>
2024-09-02 12:34:15 +0600

    using list of queues instead of arrays

--- src/GroceryQueue.java
+++ src/GroceryQueue.java
@@ -1,46 +1,25 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class GroceryQueue {
-    private final Queue<Customer> queue;
+    private List<Queue<Customer>> queues;
     private final int maxLength;
     private final int numQueues;
-    private int served = 0;
-    private int left = 0;
     private final Lock lock;
     private final Condition notEmpty;
     public GroceryQueue(int numQueues,int maxLength){
-        this.queue = new LinkedList<>();
         this.maxLength = maxLength;
         this.numQueues = numQueues;
+        this.queues = new ArrayList<>(numQueues);
+        for (int i = 0; i < numQueues; i++) {
+            queues.add(new LinkedList<>());
+        }
+        this.lock = new ReentrantLock();
+        this.notEmpty = lock.newCondition();
     }   
-    public boolean addCustomer(Customer customer) {
-        lock.lock();
-        try {
-            if (queue.size() < maxLength) {
-                queue.offer(customer);
-                return true;
-            } else {
-                return false;
-            }
-        } finally {
-            lock.unlock();
-        }
-    }
-    public Customer getNextCustomer() throws InterruptedException {
-        lock.lock();
-        try {
-            while (queue.isEmpty()) {
-                if (!queue.isEmpty()) {
-                    return queue.poll();
-                }
-            }
-            return queue.poll();
-        } finally {
-            lock.unlock();
-        }
-    }
 }
